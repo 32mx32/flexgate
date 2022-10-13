64f5a5d7584aeffb791f989b984e2c7f116addb7
32mx32 <32mx32@gmail.com>
2022-10-14 00:29:54 +0300

    add toggle title images

--- client/src/app.js
+++ client/src/app.js
@@ -6,10 +6,15 @@ import './images/icons/unchecked.svg';
 import './images/icons/telegram.svg';
 import './images/icons/instagram.svg';
 
-import './images/Group 210.jpg';
-import './images/Squircle.svg';
-import './images/maps.png';
+import './images/home/macbook.png';
+import './images/home/kenny.png';
+import './images/home/kenny2.png';
+import './images/home/kenny3.png';
+import './images/home/kenny4.png';
+import './images/home/kenny5.png';
 
+
+import './images/questions/squircle.svg';
 import './images/questions/bart_simpson.png';
 import './images/questions/doc.png';
 import './images/questions/smegol.png';
@@ -17,13 +22,27 @@ import './images/questions/storm_trooper.png';
 import './images/questions/dimon.png';
 import './images/questions/marilyn.png';
 
-
-
-
-
-
-
+import './images/maps.png';
 
 
 console.log('Hello...');
 console.log('mode', process.env.NODE_ENV);
+
+
+document.querySelectorAll('li.home_defect_button').forEach(button => {
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelectorAll('li.home_defect_button').forEach(defect_button => { defect_button.classList.remove('active') })
+        button.classList.add('active');
+
+        document.querySelectorAll('img.home_content_defect').forEach(image => { image.classList.remove('active') })
+
+        let defect = this.getAttribute('defect'); 
+        const defectImage = document.getElementById(defect);
+        defectImage.classList.add('active');
+
+        document.querySelectorAll('p.home_content_descr').forEach(description => { description.classList.remove('active') });
+        document.querySelector(`[descr='${defect}']`).classList.add('active');
+    });
+});
